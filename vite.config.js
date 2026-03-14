eafce14f67ab7f2169266f472229da06ca3e1bee
Андрей <Andrej@MacBook-Pro-Andrej.local>
2026-03-14 22:47:19 +0300

    Configure deploy for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/english-islands/',
+  base: '/english-gpt/',
   build: {
     target: 'esnext'
   }
